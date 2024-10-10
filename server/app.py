5df0558cad0e9b1de8532f2a1fe8e7a89c937230
Kush <kuriaisac@gmail.com>
2024-10-10 12:51:03 +0300

    updates changes

--- server/app.py
+++ server/app.py
@@ -22,13 +22,33 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
+    authors = [article.to_dict(rules=('-date', '-minutes_to_read', '-preview', '-user', '-user_id', )) for article in Article.query.all()]
+    response = make_response(jsonify(authors), 200)
 
-    pass
+    return response
 
 @app.route('/articles/<int:id>')
 def show_article(id):
+    # set session['page_views'] to 0 if it doesn't exist, if not leave it as it is
+    session['page_views'] = session.get('page_views', 0) + 1
 
-    pass
+    # check if the user has viewed 3 or fewer pages
+    if session['page_views'] <= 3:
+        article = {
+        "id": id,
+        "title": f"Article {id}",
+        "content": "This is the content of the article",
+        "author": "Author Name"
+        }
+        response_dict = {"article": article, "page_views": session['page_views']}
+        response = make_response(jsonify(response_dict))
+        return response
+    else:
+        response_dict = {"message": "Maximum pageview limit reached"}
+        response = make_response(jsonify(response_dict), 401)
+
+        return response
+
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5000, debug=True)
